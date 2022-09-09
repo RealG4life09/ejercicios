b09bdb9fe5a26f6a3c21ef8d0fd7b32acd01373a
RealG4life09 <113150818+RealG4life09@users.noreply.github.com>
2022-09-08 19:35:48 -0500

    Add files via upload

---
+++ eje 3.cs
@@ -0,0 +1,17 @@
+﻿namespace ejercicio3
+{
+
+    public class program2
+    {
+
+        public static void Main()
+        {
+
+            int num1;
+            double cuadrado;
+            num1 = 821;
+            cuadrado = Math.Pow(num1, 2);
+            Console.WriteLine($"el resultado del cuadrado es : {cuadrado}");
+        }
+    }
+}
